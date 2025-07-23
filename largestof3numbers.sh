481dc7f3f94f9adef4917d9eefcb2e0891fd0891
Rashmi Ramdas Pai <161941092+RashmiRamdasPai@users.noreply.github.com>
2025-07-23 18:20:35 +0530

    Create largestof3numbers.sh

---
+++ largestof3numbers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+echo "Enter the three numbers"
+read num1
+read num2
+read num3
+
+max=$num1
+if [[ $num2 -gt $max ]]
+  then
+     if [[ $num2 -gt $num3 ]]
+       then
+        echo "$num2 is the highest"
+     else
+         echo "$num2 is the highest"
+    fi
+else
+   if [[ $num1 -gt $num3 ]]
+   then
+       echo "$num1 is the highest"
+    else
+       echo "$num3 is the highest"
+    fi
+    fi
